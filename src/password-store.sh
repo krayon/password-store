4d2292aac4974cd5bf0f94c3b6f25a8f7d9bc3d9
Krayon <krayon.git@qdnx.org>
2016-02-18 01:20:53 +1100

    Add UID to sleep argv0 for multi-user systems

--- src/password-store.sh
+++ src/password-store.sh
@@ -132,7 +132,7 @@ clip() {
 	# This base64 business is because bash cannot store binary data in a shell
 	# variable. Specifically, it cannot store nulls nor (non-trivally) store
 	# trailing new lines.
-	local sleep_argv0="password store sleep on display $DISPLAY"
+	local sleep_argv0="password store for $UID sleep on display $DISPLAY"
 	pkill -f "^$sleep_argv0" 2>/dev/null && sleep 0.5
 	local before="$(xclip -o -selection "$X_SELECTION" 2>/dev/null | base64)"
 	echo -n "$1" | xclip -selection "$X_SELECTION" || die "Error: Could not copy data to the clipboard"

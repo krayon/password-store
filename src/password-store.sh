c1ee8128a5d48b06451dc6d01e23c9bd32aadb38
Krayon <krayon.git@qdnx.org>
2017-01-18 11:41:06 +1100

    Only show valid password files (.gpg)
    'pass show', 'pass find' etc use tree to list the password files and
    were also listing files that are NOT actual passwords, that is, files
    with names NOT ending in '.gpg'.

--- src/password-store.sh
+++ src/password-store.sh
@@ -332,7 +332,7 @@ cmd_show() {
 		else
 			echo "${path%\/}"
 		fi
-		tree -C -l --noreport "$PREFIX/$path" | tail -n +2 | sed -E 's/\.gpg(\x1B\[[0-9]+m)?( ->|$)/\1\2/g' # remove .gpg at end of line, but keep colors
+		tree -C -l --noreport -P "*.gpg" "$PREFIX/$path" | tail -n +2 | sed -E 's/\.gpg(\x1B\[[0-9]+m)?( ->|$)/\1\2/g' # remove .gpg at end of line, but keep colors
 	elif [[ -z $path ]]; then
 		die "Error: password store is empty. Try \"pass init\"."
 	else
@@ -343,7 +343,7 @@ cmd_show() {
 cmd_find() {
 	[[ -z "$@" ]] && die "Usage: $PROGRAM $COMMAND pass-names..."
 	IFS="," eval 'echo "Search Terms: $*"'
-	local terms="*$(printf '%s*|*' "$@")"
+	local terms="*$(printf '%s*.gpg|*' "$@")"
 	tree -C -l --noreport -P "${terms%|*}" --prune --matchdirs --ignore-case "$PREFIX" | tail -n +2 | sed -E 's/\.gpg(\x1B\[[0-9]+m)?( ->|$)/\1\2/g'
 }
 
